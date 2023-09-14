a7f808b156f0b14a958f00f37f2d71a79b8b3d5e
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-09-14 15:46:18 +0100

    feat/insert logic

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,4 +1,7 @@
-// exports.articleData = require('./articles.js');
-// exports.commentData = require('./comments.js');
-// exports.topicData = require('./topics.js');
-// exports.userData = require('./users.js');
+exports.assignmentsData = require('./assignments.js');
+exports.classes_usersData = require('./classes_users.js');
+exports.classesData = require('./classes.js');
+exports.lessonsData = require('./lessons.js');
+exports.users_assignmentsData = require('./users_assignments.js');
+exports.users_lessonsData = require('./users_lessons.js');
+exports.usersData = require('./users.js');
