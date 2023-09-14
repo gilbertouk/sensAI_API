f8dfefb98dd3aaa7dc7e979175e08d1c80cc8329
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-14 14:56:08 +0100

    users_assignments data added

--- Data/classes.js
+++ Data/classes.js
@@ -1,22 +1,7 @@
 module.exports = [
+
     {
       id: 1,
-      name: "red",
-      age_group: "KS1",
-      subject: "maths",
-      created_at: '2013-01-17',
-      exam_board: "AQA"
-    },
-    {
-      id: 2,
-      name: "blue",
-      age_group: "KS2",
-      subject: "science",
-      created_at: '2020-09-20',
-      exam_board: "WJEC"
-    },
-    {
-      id: 3,
       name: "green",
       age_group: "KS3",
       subject: "english",
@@ -24,11 +9,27 @@ module.exports = [
       exam_board: "OCR"
     },
     {
-      id: 4,
+      id: 2,
       name: "orange",
       age_group: "KS4",
       subject: "art",
       created_at: '2003-07-04',
       exam_board: "EDUQAS"
+    },
+    {
+      id: 3,
+      name: "red",
+      age_group: "KS1",
+      subject: "maths",
+      created_at: '2013-01-17',
+      exam_board: "AQA"
+    },
+    {
+      id: 4,
+      name: "blue",
+      age_group: "KS2",
+      subject: "science",
+      created_at: '2020-09-20',
+      exam_board: "WJEC"
     }
   ];
