2786b5f55501601e004ebe5e45a83c44a6781de2
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-14 10:56:14 +0100

    Add files via upload

---
+++ Data/lessons.js
@@ -0,0 +1,59 @@
+module.exports = [
+        {
+          id: 1,
+          title: "English: Shakespeare",
+          body: "An exploration of Shakespeare, his plays and books",
+          teacher_id: 101,
+          created_at: "2021-10-11",
+        },
+        {
+          id: 2,
+          title: "English: Dickens",
+          body: "A deep dive into Dickens Christmas stories",
+          teacher_id: 101,
+          created_at: "2022-09-01",
+        },
+        {
+          id: 3,
+          title: "Art: Dali",
+          body: "A exploration of surrealism and its implications for modernist art",
+          teacher_id: 101,
+          created_at: "2023-05-03",
+        },
+        {
+          id: 4,
+          title: "Art: Michelangelo",
+          body: "A consideration of how science, art, and math influence Michelangelo's creations",
+          teacher_id: 101,
+          created_at: "2019-04-03",
+        },
+        {
+          id: 5,
+          title: "Science: Einstein",
+          body: "A meditation on the space-time continuum",
+          teacher_id: 102,
+          created_at: "2018-05-03",
+        },
+        {
+          id: 6,
+          title: "Science: Darwin",
+          body: "Mankind's evolution from monkeys",
+          teacher_id: 102,
+          created_at: "2016-05-23",
+        },
+        {
+          id: 7,
+          title: "Maths: Pythagoras",
+          body: "How to work out Pythagoras' theorem",
+          teacher_id: 102,
+          created_at: "2012-01-13",
+        },
+        {
+          id: 8,
+          title: "Maths: Fibonacci",
+          body: "The Fibonacci sequence and its implications for recursion",
+          teacher_id: 102,
+          created_at: "2010-01-01",
+        },
+      ];
+            
