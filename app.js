a0a42c65b81ac92d8daf873bbfe8442821ed1a9a
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-15 13:36:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { getAllUsers } = require('./db/controllers.js');
+const app = express();
 
 const articles = require('./routes/articles.routes');
 const comments = require('./routes/comments.routes');
@@ -32,5 +34,34 @@ app.use(generalError);
 app.use(Error400);
 app.use(Error404);
 app.use(Error500);
+
+app.use(express.json())
+
+app.get('/api/users', getAllUsers)
+
+app.use((req, res) => {
+    res.status(404).send({msg: 'not found'})
+})
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({msg: 'does not exist in databse'})
+    }
+    else next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.code === "22P02" || err.code === "23503") {
+        res.status(400).send({msg: 'Invalid input'})
+    } else if (err.code === "23502") {
+        res.status(404).send({msg: 'does not exist in databse'})
+    }
+    else next(err)
+})
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).send({msg: 'server error getting API'})
+})
 
 module.exports = app;
