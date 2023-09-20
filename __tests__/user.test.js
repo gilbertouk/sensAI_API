09635bbd1b1869c6126d5ebdf53fa33653f4703f
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-09-20 14:51:17 +0100

    tests

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -8,34 +8,37 @@ beforeEach(() => seed(data));
 afterAll(() => db.end());
 
 describe("POST /api/users/newuser", () => {
-    test("201: Returns status code 201", ()=> {
-        const body = {
-            "name": "mr",
-            "surname": "bean",
-            "email": "mr@bean.com",
-            "role": "student",
-            "disability": "ADHD"
-        }
-        return request(app).post("/api/users/newuser").send(body).expect(201);
-    })
-    test("201: Returns user object", () => {
-        const body = {
-            "name": "mr",
-            "surname": "bean",
-            "email": "mr@bean.com",
-            "role": "student",
-            "disability": "ADHD"
-        }
-        return request(app).post("/api/users/newuser").send(body).expect(201).then(({body})=> {
-            const { user } = body;
-
-            const expected = {
-                
-            }
-
-        })
-
-    })
-  
-  
+  test("201: Returns status code 201", () => {
+    const body = {
+      name: "mr",
+      surname: "bean",
+      email: "mr@bean.com",
+      role: "student",
+      disability: "ADHD",
+    };
+    return request(app).post("/api/users/newuser").send(body).expect(201);
+  });
+  test("201: Returns user object", () => {
+    const body = {
+      name: "mr",
+      surname: "bean",
+      email: "mr@bean.com",
+      disability: "ADHD",
+    };
+    return request(app)
+      .post("/api/users/newuser")
+      .send(body)
+      .expect(201)
+      .then(({ body }) => {
+        const { user } = body;
+        expect(user).toEqual(
+          expect.objectContaining({
+            name: "mr",
+            surname: "bean",
+            email: "mr@bean.com",
+            disability: "ADHD",
+          })
+        );
+      });
+  });
 });
