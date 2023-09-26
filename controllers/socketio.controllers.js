f0489876135ada0640395fa5e3e4ad1acbdffada
Gilberto Silva <gilbertoantoniouk@gmail.com>
2023-09-26 20:29:49 +0100

    added chat room on socket.io

--- controllers/socketio.controllers.js
+++ controllers/socketio.controllers.js
@@ -1,11 +1,46 @@
 const { io } = require("../app");
 
+const chatMessages = [
+  {
+    room: "101_4",
+    message: [
+      {
+        text: "message text",
+        name: "User101",
+        id: "hGvYmOKTzObiUX_6AAAN0.2241069622896057",
+        socketID: "hGvYmOKTzObiUX_6AAAN",
+      },
+      {
+        text: "okay, it's work",
+        name: "User4",
+        id: "hGvYmOKTzObiUX_6AAAN0.224103239622896057",
+        socketID: "hGvYmOKTzObiUX_6AAAN",
+      },
+    ],
+  },
+];
+
 io.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
 
-  socket.on("message", (data) => {
+  socket.on("select_room", (room, sendChatHistory) => {
+    socket.join(room);
+
+    const roomMessages = findRoom(room);
+
+    if (roomMessages) {
+      sendChatHistory(roomMessages.message);
+    }
+  });
+
+  socket.on("message", ({ data, room }) => {
+    const roomMessages = findRoom(room);
     console.log(data);
-    io.emit("messageResponse", data);
+
+    if (roomMessages) {
+      roomMessages.message.push(data);
+      io.to(room).emit("messageResponse", data);
+    }
   });
 
   socket.on("disconnect", () => {
@@ -13,3 +48,11 @@ io.on("connection", (socket) => {
     socket.disconnect();
   });
 });
+
+function findRoom(room_id) {
+  const room = chatMessages.find((messages) => {
+    return messages.room === room_id;
+  });
+
+  return room;
+}
